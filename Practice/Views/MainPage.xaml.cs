5b66bd1f40c45858f11d8f2217f4920a3155a2dc
johnthebeloved <johnoluwale1@gmail.com>
2022-10-25 11:15:45 -0400

    Created functionality for the buttons

--- Practice/Views/MainPage.xaml.cs
+++ Practice/Views/MainPage.xaml.cs
@@ -8,6 +8,38 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	public void 
+	public void OnHorizontalStartClicked(object sender, EventArgs e)
+	{
+		target.HorizontalOptions = LayoutOptions.Start;
+	}
+    public void OnHorizontalCenterClicked(object sender, EventArgs e)
+    {
+        target.HorizontalOptions = LayoutOptions.Center;
+    }
+    public void OnHorizontalEndClicked(object sender, EventArgs e)
+    {
+        target.HorizontalOptions = LayoutOptions.End;
+    }
+    public void OnHorizontalFillClicked(object sender, EventArgs e)
+    {
+        target.HorizontalOptions = LayoutOptions.Fill;
+    }
+
+    public void OnVerticalStartClicked(object sender, EventArgs e)
+    {
+        target.VerticalOptions = LayoutOptions.Start;
+    }
+    public void OnVerticalCenterClicked(object sender, EventArgs e)
+    {
+        target.VerticalOptions = LayoutOptions.Center;
+    }
+    public void OnVerticalEndClicked(object sender, EventArgs e)
+    {
+        target.VerticalOptions = LayoutOptions.End;
+    }
+    public void OnVerticalFillClicked(object sender, EventArgs e)
+    {
+        target.VerticalOptions = LayoutOptions.Fill;
+    }
 }
 
